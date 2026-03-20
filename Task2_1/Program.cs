fd15a45a3f46100815aa15a54e04695f30fa0e7b
arzimanovdi <jugernaut47@gmail.com>
2026-03-20 17:02:53 +0300

    Лекция 2. Задача 1

---
+++ Task2_1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите переменную a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите переменную b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите переменную c");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int t = a;
+            int v = b;
+            a = c;
+            b = t;
+            c = v;
+            Console.WriteLine("a={0}, b={1}, c={2}", a, b, c);
+            Console.WriteLine("Введите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
