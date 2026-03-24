9dfca45e414100d3a3da7c6f809bfe70e1d196c2
arzimanovdi <jugernaut47@gmail.com>
2026-03-24 10:42:25 +0300

    Home Work 2_2 ready

---
+++ Task2_2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввод угла");
+            Console.WriteLine("Введите градусы");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите минуты");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите секунды");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Значение угла в радианах:");
+            double rad_degree = Math.PI / 180;
+            double rad_minutes = Math.PI / (180 * 60);
+            double rad_seconds = Math.PI / (180 * 60 * 60);
+            double rad = a * rad_degree + b * rad_minutes + c * rad_seconds;
+            Console.WriteLine("{0}", rad);
+            Console.WriteLine("Нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
