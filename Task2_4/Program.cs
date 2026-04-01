ba2e076b05cad0e9f156c67f19352e14c248c8d0
arzimanovdi <jugernaut47@gmail.com>
2026-04-01 16:54:34 +0300

    Задание 1.2.4 добавлено.

---
+++ Task2_4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task2_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1.2.4: Вводится четырехзначное число. Поменять местами 2-ую и 4-ую цифру.
+            Console.WriteLine("Введите четырехзначное число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int first = a / 1000;
+            int second = a / 100 % 10;
+            int third = a % 100 / 10;
+            int fourth = a % 10;
+            Console.WriteLine("{0}{1}{2}{3}", first, fourth, third, second);
+            Console.WriteLine("Нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
