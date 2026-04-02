e0702fb995887ed0e8d8fa0da7daa54231e00c69
arzimanovdi <jugernaut47@gmail.com>
2026-04-02 22:44:18 +0300

    Задание 1.2.5 добавлено

---
+++ Task2_5/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task2_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вводится длина трубы в метрах. Округлить длину до целых в большую сторону. Например, 
+
+            // 17,0-> 17
+
+            // 17,3-> 18
+
+            // 17,7-> 18
+            Console.WriteLine("Введите длину трубы в метрах");
+            double TubingLength = Convert.ToDouble(Console.ReadLine());
+            TubingLength = Math.Round(TubingLength,0,MidpointRounding.ToPositiveInfinity);
+            Console.WriteLine("Длина трубы = {0} м", TubingLength);
+            Console.WriteLine("Нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
