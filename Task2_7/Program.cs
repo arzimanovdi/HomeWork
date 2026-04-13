19739247d7842498cc9dd7ac882959f685c9be2a
arzimanovdi <jugernaut47@gmail.com>
2026-04-13 13:41:04 +0300

    Задание 1.2.7 добавлено

---
+++ Task2_7/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task2_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вводятся два числа. Обменять их местами, не используя третью переменную.
+            Console.WriteLine("Введите значения переменных");
+            int a = Convert.ToInt32(Console.ReadLine()); 
+            int b = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("переменная a = {0}", a);
+            Console.WriteLine("переменная b = {0}", b);
+            a = b + a; 
+            b = a - b; 
+            a = a - b;
+            Console.WriteLine("переменная а = {0}, переменная b = {1}", a, b);
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
