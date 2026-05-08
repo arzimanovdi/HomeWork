e47f9e79ee565891570f35ec8838881cd61677cc
arzimanovdi <jugernaut47@gmail.com>
2026-05-08 08:40:57 +0300

    Добавлены задания темы 4 : Циклы

---
+++ Task4_2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вводится натуральное число n. Найти 1 + 1/2 + 1/3 + … + 1/n
+            int N;
+            double s = 1.0;
+            Console.WriteLine("Введите натуральное число");
+            N = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i <= N; i++)
+            {
+                s = s + 1.0 / i;
+            }
+            Console.WriteLine("Ответ: {0}", s);
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task4_4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task4_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вводятся целые числа a>0, b<0. Найти a^b. Не использовать класс Math
+            int a;
+            int b;
+            double s = 1.0;
+            Console.Write("Введите целое число a>0:  ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите целое число b<0:  ");
+            b = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= b*(-1); i++)
+            {
+                s = s * a;
+            }
+            s = 1.0 / s;
+            Console.WriteLine("Ответ: {0}", s);
+            Console.WriteLine("Нажмите любую клавишу для продолжения...");
+            Console.ReadKey();
+        }
+    }
+}
